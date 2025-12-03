067a2e69c6c8433cb23839ee3d83f07261d18aa7
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-12-03 12:05:53 +0500

    chore: инициализация next
    --APP: инициализирован next.js
    --.prettierrc: добавлены настройки линтера
    --.stylelintrc.json: добавлены настройки линтера для css и scss
    --imgs/next-js.webp: удален каталог
    --next-js.webp: перемещен в каталог public/imgs/webp

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import '@styles/globals.css';
+
+const HomePage = () => {
+	return (
+		<div className="w-full h-screen bg-black">
+			<h1 className="text-white font-bold text-4xl">Home Page</h1>
+		</div>
+	);
+};
+
+export default HomePage;
